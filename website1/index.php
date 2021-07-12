8cd3ab6c7d3169184cb340ece287126d831c14e1
hexhax7 <87332576+hexhax7@users.noreply.github.com>
2021-07-12 18:42:16 +0100

    Add files via upload

---
+++ website1/index.php
@@ -0,0 +1,17 @@
+<?php 
+if (isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $subject = $_POST["subject"];
+    $mail = $_POST["mail"];
+    $message = $_POST["message"];
+ 
+    $mailTo = "dani@mmtuts.net";
+    $headers = "From: ".$mailFrom;
+    $txt = "you have received an email from".$name.".\n\n".$message;
+
+    mail($mailTo,$subject,$txt,$headers);
+    header("location: index.php?mailsend");
+}
+
+
+
